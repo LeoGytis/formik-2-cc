6470b22c30a77f9b23623e3092e732a4c41a14e3
Gytis <101097792+LeoGytis@users.noreply.github.com>
2022-11-17 11:57:59 +0200

    again

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { Formik } from "formik";
+import { TextField } from "@material-ui/core";
 
 function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
+    <>
+      <Formik
+        initialValues={{ firstName: "bob" }}
+        onSubmit={(data) => {
+          console.log(data);
+        }}
+      >
+        {({ values, handleChange, handleBlur, handleSubmit }) => (
+          <form onSubmit={handleSubmit}>
+            <TextField
+              name="firstName"
+              value={values.firstName}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+          </form>
+        )}
+      </Formik>
+    </>
   );
 }
 
