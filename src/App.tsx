8d637a549a8b4dc068fd305dbcd4537aa8441eff
Gytis <101097792+LeoGytis@users.noreply.github.com>
2022-11-17 12:39:50 +0200

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 import React from "react";
-import { Formik } from "formik";
-import { TextField } from "@material-ui/core";
+import { Formik, Field } from "formik";
+import { Button, TextField } from "@material-ui/core";
 
 function App() {
   return (
     <>
       <Formik
-        initialValues={{ firstName: "bob" }}
-        onSubmit={(data) => {
-          console.log(data);
+        initialValues={{ firstName: "Gytis", lastName: "Leo" }}
+        onSubmit={(data, { setSubmitting }) => {
+          setSubmitting(true);
+          console.log('Submit: ', data);
+          setSubmitting(false);
         }}
       >
-        {({ values, handleChange, handleBlur, handleSubmit }) => (
+        {({ values, isSubmitting, handleChange, handleBlur, handleSubmit }) => (
           <form onSubmit={handleSubmit}>
-            <TextField
-              name="firstName"
-              value={values.firstName}
-              onChange={handleChange}
-              onBlur={handleBlur}
-            />
+            <Field name="firstName" type="input" as={TextField} />
+            <Field name="lastName" type="input" as={TextField} />
+            <div>
+              <Button disabled={isSubmitting} type="submit">Submit</Button>
+            </div>
+            <pre>{JSON.stringify(values, null, 2)}</pre>
           </form>
         )}
       </Formik>
